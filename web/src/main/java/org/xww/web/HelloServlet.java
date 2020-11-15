296ecea4663f20de91ccb3cdd6c6b28e3f16841d
徐卫卫 <xuweiwei@seccloud.cn>
2020-11-15 10:57:33 +0800

    first commit

---
+++ web/src/main/java/org/xww/web/HelloServlet.java
@@ -0,0 +1,23 @@
+package org.xww.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        req.setAttribute("currentTime", currentTime);
+        req.getRequestDispatcher("/jsp/hello.jsp").forward(req,
+                resp);
+    }
+}
---
+++ web/target/web-1.0-SNAPSHOT/jsp/hello.html
@@ -0,0 +1,14 @@
+<%@ page pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+
+<html lang="en">
+<script src="js/test.js"></script>
+<head>
+    <meta charset="UTF-8">
+    <title>Hello</title>
+</head>
+<body>
+<h1>Hello!</h1>
+<h2>当前时间:${currentTime}</h2>
+</body>
+</html>
